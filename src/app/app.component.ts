17d1b900b51f14185657191d14eb4e8e518e80a5
luisangelc <la.castillo09@info.uas.edu.mx>
2021-03-15 15:04:51 -0700

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: `<span>Hola</span>`
 })
 export class AppComponent {
-  title = 'bases';
+
+
 }
